7dd7967d7de0a7fc33a44cfee40805cc0f51f2d0
Roshan0310 <roshanrosh0310@gmail.com>
2024-01-03 21:49:25 +0530

    03-01-2024

--- 03-01-2024/intro.java
+++ 03-01-2024/intro.java
@@ -1,6 +1,13 @@
 // First program on java
 class Intro {
 	public static void main(String[] args) {
-		System.out.println("Hello roshan , you are doing great..!");
+    // printing something
+		System.out.println("Hello Roshan");
+		System.out.println("Your are doing great");
+
+    // variables
+        int firstVariable; // Declaring varibales
+		firstVariable = 23; // Intialized
+		System.out.println(firstVariable); // output = 23
 	}
 }
